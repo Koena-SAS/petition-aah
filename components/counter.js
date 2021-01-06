d9793d90aea6d843d2de959f0a25ea7c293d0214
Roman Mkrtchian <roman@koena.net>
2021-01-06 18:17:48 +0100

    feat: make the signatures counter animated and fetch queries logarithmically less often

--- components/counter.js
+++ components/counter.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import styles from "../styles/Counter.module.scss";
 import PropTypes from "prop-types";
 import axios from "axios";
@@ -6,45 +6,133 @@ import axios from "axios";
 /**
  * Counter for the AAH senate 416 initiative.
  *
- * Fetches and displays the number of people
- * supporting the initiative, updating regularly.
+ * Fetches and displays the number of people supporting the initiative,
+ * updating regularly.
+ * Updates are configurable with constants in this file. They are done less
+ * and less quickly until a limit value.
  */
-export default function Counter({ value }) {
+export default function Counter({
+  value,
+  initialTimeToWait,
+  growingFactor,
+  growingLimit,
+  maxTotalRequests,
+  countAnimationFrames,
+  countAnimationDelay,
+}) {
   const [countValue, setCountValue] = useState(value);
-  const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+  const [delay, setDelay] = useState(initialTimeToWait);
+  const waitTimeGenerator = useRef(
+    generateNextWaitTime(initialTimeToWait, growingFactor, growingLimit)
+  );
+  const totalRequests = useRef(0);
 
   useEffect(() => {
     async function updateInitiative() {
-      while (true) {
-        axios
-          .get(`https://petitions.senat.fr/initiatives/i-416`)
-          .then((response) => {
-            const parser = new DOMParser();
-            const data = parser.parseFromString(response.data, "text/html");
-            const fetchedValue = data.querySelector(
+      // We are querying/parsing the html page to get the count value but the
+      // senate website has an API, and when they update their Decidim instance
+      // we will be able to query the GraphQL API directly to get that value
+      // cf. https://github.com/decidim/decidim/discussions/7080
+      axios
+        .get(`https://petitions.senat.fr/initiatives/i-416`)
+        .then(async (response) => {
+          const parser = new DOMParser();
+          const data = parser.parseFromString(response.data, "text/html");
+          const fetchedValue = parseInt(
+            data.querySelector(
               "#initiative-416-votes-count span.progress__bar__number"
-            ).textContent;
-            setCountValue(fetchedValue);
-          })
-          .catch(() => {});
-        await delay(3000);
-        console.log("fetch done");
+            ).textContent
+          );
+          console.log(fetchedValue);
+          await setCountValueProgressively(fetchedValue);
+          await waitDelay(delay);
+          totalRequests.current += 1;
+          if (totalRequests.current < maxTotalRequests) {
+            setDelay(waitTimeGenerator.current.next().value);
+          }
+        })
+        .catch(() => {});
+    }
+    async function setCountValueProgressively(fetchedValue) {
+      if (fetchedValue > countValue) {
+        if (!countAnimationFrames) {
+          setCountValue(fetchedValue);
+        } else {
+          let increment =
+            (fetchedValue - countValue - 0.1) / countAnimationFrames;
+          let i = countValue;
+          const interval = setInterval(() => {
+            if (i > fetchedValue) {
+              clearInterval(interval);
+            } else {
+              setCountValue(Math.ceil(i));
+            }
+            i += increment;
+          }, countAnimationDelay);
+        }
       }
     }
-    updateInitiative();
-  }, []);
+    if (totalRequests.current < maxTotalRequests) {
+      updateInitiative();
+    }
+  }, [delay]); // eslint-disable-line react-hooks/exhaustive-deps
 
   return <div className={styles.counter}>{countValue}</div>;
 }
 
 Counter.propTypes = {
   /**
-   * Used to set initial value before the first fetch
-   * if not given, 0 will be used
+   * Used to set initial value before the first fetch.
    */
   value: PropTypes.number,
+  /**
+   * The initial time to wait before the next fetch of the counter value
+   * (in miliseconds)
+   */
+  initialTimeToWait: PropTypes.number,
+  /**
+   * The factor multiplying the initial time after each fetch.
+   */
+  growingFactor: PropTypes.number,
+  /**
+   * The time value above which the time to wait between two fetches will
+   * no more grow (in miliseconds)
+   */
+  growingLimit: PropTypes.number,
+  /**
+   * This is for security, 150 is enouth with the other values to have requests
+   * until the end of the petition
+   */
+  maxTotalRequests: PropTypes.number,
+  /**
+   * The maximum number of of updates that will be used to animate the counter.
+   */
+  countAnimationFrames: PropTypes.number,
+  /**
+   * The time to wait between each updates that will be used to animate the counter.
+   */
+  countAnimationDelay: PropTypes.number,
 };
 
 Counter.defaultProps = {
   value: 0,
+  initialTimeToWait: 3000,
+  growingFactor: 1.2,
+  growingLimit: 86400000,
+  maxTotalRequests: 150,
+  countAnimationFrames: 20,
+  countAnimationDelay: 30,
 };
+
+async function waitDelay(ms) {
+  return new Promise((res) => setTimeout(res, ms));
+}
+
+function* generateNextWaitTime(initialTimeToWait, growingFactor, growingLimit) {
+  let time = initialTimeToWait;
+  while (true) {
+    const nextTime = time * growingFactor;
+    time = nextTime < growingLimit ? nextTime : growingLimit;
+    yield time;
+  }
+}
--- components/counter.test.js
+++ components/counter.test.js
@@ -29,13 +29,17 @@ afterEach(() => {
 
 describe("Initial behavior", () => {
   it("displays the given value at the beginning", async () => {
-    const { getByText } = render(<Counter value={100} />);
+    const { getByText } = render(
+      <Counter value={100} countAnimationFrames={0} maxTotalRequests={1} />
+    );
     expect(getByText("100")).toBeInTheDocument();
     await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(1));
   });
 
   it("displays 0 at the beginning if no value provided", async () => {
-    const { getByText } = render(<Counter />);
+    const { getByText } = render(
+      <Counter countAnimationFrames={0} maxTotalRequests={1} />
+    );
     expect(getByText("0")).toBeInTheDocument();
     await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(1));
   });
@@ -43,8 +47,59 @@ describe("Initial behavior", () => {
 
 describe("Fetching value", () => {
   it("fetches and displays counter value", async () => {
-    const { getByText } = render(<Counter />);
+    const { getByText } = render(
+      <Counter countAnimationFrames={0} maxTotalRequests={1} />
+    );
     await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(1));
     expect(getByText("500")).toBeInTheDocument();
   });
+
+  it("refetches and displays changed counter value", async () => {
+    const { getByText } = render(
+      <Counter
+        countAnimationFrames={0}
+        maxTotalRequests={2}
+        initialTimeToWait={10}
+      />
+    );
+    await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(1));
+    expect(getByText("500")).toBeInTheDocument();
+    const senatGetResponse = {
+      data: `<div id="initiative-416-votes-count" class="progress__bar progress__bar--vertical">
+    <div class="progress__bar__title">
+  
+            <span class="progress__bar__number">653</span>/100000      <span class="progress__bar__text">SIGNATURES</span>
+        </div>
+        <div class="progress progress__bar__bar" role="progressbar" tabindex="0" aria-valuenow="39.421" aria-valuemin="0" aria-valuetext="39.421 percent" aria-valuemax="100">
+        <div class="progress-meter progress__bar__bar--complete" style="width: 39.421%"></div>
+        <div class="progress__bar__bar--incomplete" style="width:calc(100% - 39.421%);"></div>
+      </div>
+  
+            <div class="progress__bar__subtitle">
+          Besoin de plus de signatures      </div>
+        </div>`,
+    };
+    axios.get.mockResolvedValue(senatGetResponse);
+    await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(2));
+    expect(getByText("653")).toBeInTheDocument();
+  });
 });
+
+describe("Security tests", () => {
+  it("ends after maxTotalRequests", async () => {
+    render(
+      <Counter
+        countAnimationFrames={0}
+        maxTotalRequests={10}
+        initialTimeToWait={1}
+      />
+    );
+    await waitFor(() => expect(axios.get).toHaveBeenCalledTimes(10));
+    await waitDelay(100);
+    expect(axios.get).toHaveBeenCalledTimes(10);
+  });
+});
+
+async function waitDelay(ms) {
+  return new Promise((res) => setTimeout(res, ms));
+}
