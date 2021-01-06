417edcd84b3f41afc92bc67f8307df2dde920b10
Roman Mkrtchian <roman@koena.net>
2021-01-06 08:28:19 +0100

    maint: solve css files ignoring in tests in a more elegant way

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,14 @@
 module.exports = {
-  testPathIgnorePatterns: ["<rootDir>/.next/", "<rootDir>/node_modules/"],
+  testPathIgnorePatterns: [
+    "<rootDir>/.next/",
+    "<rootDir>/out/",
+    "<rootDir>/node_modules/",
+  ],
   setupFilesAfterEnv: ["<rootDir>/setupTests.js"],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
-    "\\.(css|less|sass|scss)$": "<rootDir>/test/mock/styleMock.js",
+  },
+  moduleNameMapper: {
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
   },
 };
--- test/mock/styleMock.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  // this function is used to transform css
-  // files to nothing during unit tests
-  process: function () {
-    return "";
-  },
-};
