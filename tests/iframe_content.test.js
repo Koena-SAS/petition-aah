c469ac430c8156f7bedd93119d2f0d0bc3d1e5f3
Roman Mkrtchian <roman@koena.net>
2021-01-15 16:27:00 +0100

    test: add banner format test for iframe content

--- tests/iframe_content.test.js
+++ tests/iframe_content.test.js
@@ -1,7 +1,7 @@
 import { render } from "@testing-library/react";
 import IframeContent from "../components/iframe_content";
 
-it("displays main elements", () => {
+it("displays main elements with block format", () => {
   const { getByText } = render(<IframeContent format="block" />);
   const dateCounter = getByText("jours");
   expect(dateCounter).toBeInTheDocument();
@@ -9,4 +9,12 @@ it("displays main elements", () => {
   expect(signaturesCounter).toBeInTheDocument();
   const buttonSign = getByText(/Signez/);
   expect(buttonSign).toBeInTheDocument();
+});
+
+it("displays main elements with banner format", () => {
+  const { getByText } = render(<IframeContent format="banner" />);
+  const dateCounter = getByText(/J-/);
+  expect(dateCounter).toBeInTheDocument();
+  const signaturesCounter = getByText(/je signe !/);
+  expect(signaturesCounter).toBeInTheDocument();
 });
