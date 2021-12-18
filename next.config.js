3ce8da0d98d385696a02a8ed848f9362ea0418a1
Roman Mkrtchian <roman@koena.net>
2021-12-18 23:46:30 +0100

    maint: host the built static version on github pages

--- next.config.js
+++ next.config.js
@@ -1,3 +1,8 @@
+const prefix = process.env.NEXT_PUBLIC_BASE_PATH;
+console.log("prefix ", prefix);
+
 module.exports = {
   trailingSlash: true,
+  assetPrefix: prefix ? prefix : "",
+  basePath: prefix ? prefix : "",
 };
